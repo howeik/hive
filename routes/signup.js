89c294593ee24ff97b6bd6b192b6bec2956833d5
Ashley Perez <amp016@ucsd.edu>
2015-02-20 11:39:21 +0000

    Implemented Sign Up

--- routes/signup.js
+++ routes/signup.js
@@ -4,8 +4,22 @@ exports.view = function(req, res){
   res.render('signup');
 };
 
-exports.signup = function(req, res){
+exports.adduser = function(req, res){
   console.log(req.body);
 
-  	var name = req.body.username;
+  	var name = req.body.name;
+  	var email = req.body.email;
+  	var password = req.body.password;
+
+  	console.log(name);
+  	console.log(email);
+  	console.log(password);
+
+  	var newUser = new models.User({'name':name, 'email':email, 'password':password});
+  	newUser.save(afterSave);
+
+  	function afterSave(err) {
+    	if (err) { console.log(err); res.send(500); return;}
+    	res.redirect('login');
+ 	}
 };
