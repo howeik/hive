3569583a2bec0cccf6ce82dcb8e820b3c3c5d19d
Ashley Perez <amp016@ucsd.edu>
2015-02-20 08:59:34 +0000

    added a little

--- routes/signup.js
+++ routes/signup.js
@@ -1,3 +1,11 @@
+var models = require('../models');
+
 exports.view = function(req, res){
   res.render('signup');
+};
+
+exports.signup = function(req, res){
+  console.log(req.body);
+
+  	var name = req.body.username;
 };
