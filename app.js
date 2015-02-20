c45d73a84da7d401259b8c3c1cc1d2593e11f616
Ho-Wei Kang <howeik@gmail.com>
2015-02-19 22:41:57 -0800

    set up mongodb/models/seed data

--- app.js
+++ app.js
@@ -2,11 +2,20 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 var handlebars = require('express3-handlebars')
+var mongoose = require('mongoose');
 
 var index = require('./routes/index');
 // Example route
 // var user = require('./routes/user');
 var login = require('./routes/login');
+
+
+// Connect to the Mongo database, whether locally or on Heroku
+// MAKE SURE TO CHANGE THE NAME FROM 'lab7' TO ... IN OTHER PROJECTS
+var local_database_name = 'hive';
+var local_database_uri  = 'mongodb://localhost/' + local_database_name
+var database_uri = process.env.MONGOLAB_URI || local_database_uri
+mongoose.connect(database_uri);
 
 var app = express();
 
