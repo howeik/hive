f4c352c33eec4c6d398095bb884a497f10998808
Ashley Perez <amp016@ucsd.edu>
2015-02-27 08:13:53 +0000

    User is redirected to tasks list page after signing up

--- routes/signup.js
+++ routes/signup.js
@@ -20,6 +20,61 @@ exports.adduser = function(req, res){
 
   	function afterSave(err) {
     	if (err) { console.log(err); res.send(500); return;}
-    	res.redirect('login');
+    	//res.redirect('login');
+      var email = req.body.email.toLowerCase();
+  var password = req.body.password;
+
+  models.User.find({ 'email': email }).exec(function(err, users) {
+    if (err) { console.log(err); res.send(500); }
+
+    var success = false;
+    var user_id = null;
+    if (users.length >= 1) {
+      var correctPassword = users[0].password;
+      if (correctPassword == password) {
+        success = true;
+        user_id = users[0]._id;
+      }
+    }
+
+    if (success) {
+      res.cookie('user_id', user_id, {signed: true})
+      console.log("user of user_id " + user_id + " signed in!");
+      res.redirect('/');
+    } else {
+      req.flash('message', 'Invalid email or password!');
+      res.redirect('login');
+    }
+  });
  	}
 };
+
+// exports.auth = function(req, res) {
+//   console.log(req.body);
+
+//   var email = req.body.email.toLowerCase();
+//   var password = req.body.password;
+
+//   models.User.find({ 'email': email }).exec(function(err, users) {
+//     if (err) { console.log(err); res.send(500); }
+
+//     var success = false;
+//     var user_id = null;
+//     if (users.length >= 1) {
+//       var correctPassword = users[0].password;
+//       if (correctPassword == password) {
+//         success = true;
+//         user_id = users[0]._id;
+//       }
+//     }
+
+//     if (success) {
+//       res.cookie('user_id', user_id, {signed: true})
+//       console.log("user of user_id " + user_id + " signed in!");
+//       res.redirect('/');
+//     } else {
+//       req.flash('message', 'Invalid email or password!');
+//       res.redirect('login');
+//     }
+//   });
+// };
